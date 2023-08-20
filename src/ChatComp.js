0769ea791bff15bae4f28683b6b291b7656ac151
VIRAL PATEL <viral9411@gmail.com>
2023-08-20 09:47:51 +0530

    a

---
+++ src/ChatComp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ChatComp.css";
+
+function ChatComp() {
+  return (
+    <div className='chatcomp'>
+        <h1>cccc</h1>
+    </div>
+  )
+}
+
+export default ChatComp
---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+    <h1>ssssss</h1>
+    <div className="sidebar__header">
+      <div className="sidebar__headerRight">
+        
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Sidebar
